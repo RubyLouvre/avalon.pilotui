37bb03d91c26baf88ef29b341e7acd93102625b4
RubyLouvre <cheng19840218@gmail.com>
2014-04-10 19:20:25 +0800

    update

--- avalon.progressbar.js
+++ avalon.progressbar.js
@@ -1,29 +1,19 @@
 define(["avalon"], function(avalon) {
 
     var widget = avalon.ui.progressbar = function(element, data, vmodels) {
-        var $element = avalon(element), options = data.progressbarOptions, model, el
-        $element.addClass("ui-progressbar ui-widget ui-widget-content ui-corner-all")
-        element.innerHTML = '<div class="ui-progressbar-value ui-widget-header ui-corner-left ui-corner-right" ms-attr-id="\'progressbar' + data.progressbarId + '\'" style="width:' + options.value + '%;"></div>'
-        var fragment = document.createDocumentFragment()
-        while (el = element.firstChild) {
-            fragment.appendChild(el)
-        }
-        model = avalon.define(data.progressbarId, function(vm) {
-            vm.value = options.value
-            vm.$watch("value", function(v) {
-                if (isFinite(v)) {
-                    var el = document.getElementById("progressbar" + model.$id)
-                    if (el) {
-                        el.style.width = v + "%"
-                    }
-                }
-            })
+        var options = data.progressbarOptions
+        var vmodel = avalon.define(data.progressbarId, function(vm) {
+            avalon.mix(vm, options)
+            vm.$init = function() {
+                avalon(element).addClass("ui-progressbar ui-widget ui-widget-content ui-corner-all")
+                element.innerHTML = '<div class="ui-progressbar-value ui-widget-header ui-corner-left" ms-css-width="{{value}}%"></div>'
+                avalon.scan(element, [vmodel].concat(vmodels))
+            }
+            vm.$remove = function() {
+                element.innerHTML = ""
+            }
         })
-        avalon.nextTick(function() {
-            element.appendChild(fragment)
-            avalon.scan(element, [model].concat(vmodels))
-        })
-        return model
+        return vmodel
     }
     widget.defaults = {
         value: 0

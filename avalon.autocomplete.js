4dcebdfb0cea81b0594757d9d6513f4b53591d7a
RubyLouvre <cheng19840218@gmail.com>
2014-04-10 18:29:04 +0800

    update autocomplete

--- avalon.autocomplete.js
+++ avalon.autocomplete.js
@@ -1,83 +1,29 @@
-define(["avalon"], function(avalon) {
+define(["avalon", "text!avalon.autocomplete.html"], function(avalon, sourceHTML) {
 
-    var widget = avalon.ui.autocomplete = function(element, data, vmodels, opts) {
+    var widget = avalon.ui.autocomplete = function(element, data, vmodels) {
 
         var $element = avalon(element),
                 refreshList,
-                tempValue = "",
-                model
+                tempValue = ""
         //处理配置
         var options = data.autocompleteOptions
         var source = options.source || []
-        var sourceList = document.createElement("div")
-        sourceList.innerHTML = '<ul  class="ui-autocomplete ui-front ui-menu ui-widget ui-widget-content ui-corner-all" ms-each-presentation="matcher" ms-visible="show" >' +
-                '<li  class="ui-menu-item" >' +
-                '<a class="ui-corner-all" tabindex="-1" ' +
-                'ms-mouseover="get(presentation)" ms-hover="ui-state-focus" ms-class="ui-state-focus:matcher[selectedIndex] === presentation "' +
-                '  >{{presentation}}</a></li>' +
-                '</ul>'
 
-        sourceList = sourceList.firstChild
-        $element.bind("blur", function() {
-            setTimeout(function() {
-                refreshList = model.show = false //隐藏datalist
-            }, 250)
-        })
-        avalon.bind(sourceList, "click", function() {
-            model.value = model.overvalue
-        })
-        $element.bind("keyup", function(e) {
-            if (/\w/.test(String.fromCharCode(e.which))) { //如果是字母数字键
-                refreshList = true //这是方便在datalist显示时,动态刷新datalist
-                model.value = element.value //触发$watch value回调
-            } else {
-                refreshList = false
-                switch (e.which) {
-                    case 8:
-                        refreshList = true//回退键可以引发列表刷新
-                        break
-                    case 13:
-                        tempValue = model.value
-                        refreshList = model.show = false
-                        break
-                    case 38:
-                        // up arrow
-                        --model.selectedIndex
-                        if (model.selectedIndex === -2) {
-                            model.selectedIndex = model.matcher.length - 1
-                        }
-                        var value = model.matcher[model.selectedIndex]
-                        model.value = value === void 0 ? tempValue : value
-                        break
-
-                    case 40:
-                        // down arrow
-                        ++model.selectedIndex
-                        if (model.selectedIndex === model.matcher.length) {
-                            model.selectedIndex = -1
-                        }
-                        var value = model.matcher[model.selectedIndex]
-                        model.value = value === void 0 ? tempValue : value
-                        break
-                }
-            }
-
-        })
-
-        model = avalon.define(data.autocompleteId, function(vm) {
+        var vmodel = avalon.define(data.autocompleteId, function(vm) {
+            avalon.mix(vm, options)
             vm.show = false
             vm.selectedIndex = -1
             vm.value = element.value
             vm.matcher = []
             vm.source = source
-            vm.skipArray= ["source"]
+            vm.skipArray = ["source"]
             vm.overvalue = ""
             vm.get = function(value) {
                 vm.overvalue = value
             }
             vm.$watch("value", function(value) {
                 if (refreshList !== false) { //flagKeyup是控制datalist的刷新
-                    model.show = true
+                    vmodel.show = true
                     tempValue = value
                     var lower = []
                     var matcher = vm.source.filter(function(el) {
@@ -100,29 +46,78 @@ define(["avalon"], function(avalon) {
                     }
                 }
             })
-        })
-        avalon.ready(function() {
-            element.setAttribute("ms-duplex", "value")
-            document.body.appendChild(sourceList)
-            adjustPosition()
-            var models = [model].concat(vmodels)
-            avalon.scan(element, models)
-            avalon.scan(sourceList, models)
+            vm.$init = function() {
+                var sourceList = avalon.parseHTML(sourceHTML).firstChild
+                avalon.bind(sourceList, "click", function() {
+                    vmodel.value = vmodel.overvalue
+                })
+                function adjustPosition() {
+                    var offset = $element.offset()
+                    sourceList.style.width = element.clientWidth + "px"
+                    sourceList.style.left = offset.left + "px"
+                    sourceList.style.top = offset.top + element.offsetHeight + "px"
+                    sourceList.style.zIndex = 9999
+                    if (avalon(document).height() - 200 > offset.top) {
+                        var pageY = sourceList.offsetHeight + parseFloat(sourceList.style.top)
+                        window.scrollTo(pageY + 50, 0)
+                    }
+                }
+                $element.bind("focus", adjustPosition)
+                $element.bind("blur", function() {
+                    setTimeout(function() {
+                        refreshList = vmodel.show = false //隐藏datalist
+                    }, 250)
+                })
+                $element.bind("keyup", function(e) {
+                    if (/\w/.test(String.fromCharCode(e.which))) { //如果是字母数字键
+                        refreshList = true //这是方便在datalist显示时,动态刷新datalist
+                        vmodel.value = element.value //触发$watch value回调
+                    } else {
+                        refreshList = false
+                        switch (e.which) {
+                            case 8:
+                                refreshList = true//回退键可以引发列表刷新
+                                break
+                            case 13:
+                                tempValue = vmodel.value
+                                refreshList = vmodel.show = false
+                                break
+                            case 38:
+                                // up arrow
+                                --vmodel.selectedIndex
+                                if (vmodel.selectedIndex === -2) {
+                                    vmodel.selectedIndex = vmodel.matcher.length - 1
+                                }
+                                var value = vmodel.matcher[vmodel.selectedIndex]
+                                vmodel.value = value === void 0 ? tempValue : value
+                                break
+
+                            case 40:
+                                // down arrow
+                                ++vmodel.selectedIndex
+                                if (vmodel.selectedIndex === vmodel.matcher.length) {
+                                    vmodel.selectedIndex = -1
+                                }
+                                var value = vmodel.matcher[vmodel.selectedIndex]
+                                vmodel.value = value === void 0 ? tempValue : value
+                                break
+                        }
+                    }
+                })
+
+
+                avalon.ready(function() {
+                    element.setAttribute("ms-duplex", "value")
+                    document.body.appendChild(sourceList)
+                    adjustPosition()
+                    var models = [vmodel].concat(vmodels)
+                    avalon.scan(element, models)
+                    avalon.scan(sourceList, models)
+                })
+            }
         })
 
-        function adjustPosition() {
-            var offset = $element.offset()
-            sourceList.style.width = element.clientWidth + "px"
-            sourceList.style.left = offset.left + "px"
-            sourceList.style.top = offset.top + element.offsetHeight + "px"
-            sourceList.style.zIndex = 9999
-            if (avalon(document).height() - 200 > offset.top) {
-                var pageY = sourceList.offsetHeight + parseFloat(sourceList.style.top)
-                window.scrollTo(pageY + 50, 0)
-            }
-        }
-        $element.bind("focus", adjustPosition)
-        return model
+        return vmodel
     }
     widget.defaults = {
         items: 8
